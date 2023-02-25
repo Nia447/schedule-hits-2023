5c09f7e5620814fc4a4cd40943196bbf9d524d5d
Naubetov <naubetove428@gmail.com>
2023-02-25 18:34:37 +0700

    Idetrash

--- app/src/main/java/com/example/schedule/MainActivity.kt
+++ app/src/main/java/com/example/schedule/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.яыяыяыыыыя
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
