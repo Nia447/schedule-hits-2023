a3ae65b0fc3d0dff7f698bb2dc66750a38276ec3
Naubetov <naubetove428@gmail.com>
2023-02-27 22:46:25 +0700

    -add intents
    -add intent date for studentact
    -add intent date for teacheract
    -add buttons for choise

--- app/src/main/java/com/example/schedule/userchoise.kt
+++ app/src/main/java/com/example/schedule/userchoise.kt
@@ -1,11 +1,23 @@
 package com.example.schedule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class userchoise : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_userchoise)
+    }
+    public fun studentact(view: View){
+        val studentInt = Intent(this,Student::class.java)
+        startActivity(studentInt)
+
+    }
+    public fun teacheract(view: View){
+        val prepodint = Intent(this,prepod::class.java)
+        startActivity(prepodint)
+
     }
 }
