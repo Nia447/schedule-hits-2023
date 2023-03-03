49da442f7cf90eaf32f33d73a357513b9513646c
qayum001 <99470584+qayum001@users.noreply.github.com>
2023-03-03 17:22:54 +0700

    Selection controller and service added
    [GET] teachers
    [GET] groups
    [GET] subjects
    [GET] audiences

---
+++ Backend/Schedule/Controllers/SelectionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Schedule.Data.Models.DTO;
+using Schedule.Services;
+
+namespace Schedule.Controllers
+{
+	[ApiController]
+	[Route("api")]
+	public class SelectionController : ControllerBase
+	{
+		private readonly ISelectionService _selectionService;
+
+		public SelectionController(ISelectionService selectionService)
+		{
+			_selectionService = selectionService;
+		}
+
+		[HttpGet("teachers")]
+		public ActionResult<TeacherListDto> GetTeachersList(string searchStr)
+        {
+			return _selectionService.SelectTeachersBySearchStr(searchStr);
+        }
+
+		[HttpGet("groups")]
+		public ActionResult<TeacherListDto> GetGroupsList(string searchStr)
+		{
+			return _selectionService.SelectTeachersBySearchStr(searchStr);
+		}
+		
+		[HttpGet("Audieces")]
+		public ActionResult<AudienceListDto> GetAudiencesList(string searchStr)
+		{
+			return _selectionService.SelectAudienceBySearchStr(searchStr);
+		}
+
+		[HttpGet("subjects")]
+		public ActionResult<SubjectListDto> GetSubjctsList(string searchStr)
+		{
+			return _selectionService.SelectSubjectBySearchStr(searchStr);
+		}
+	}
+}
--- Backend/Schedule/Program.cs
+++ Backend/Schedule/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Schedule.Data;
+using Schedule.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddSwaggerGen();
 // Database
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ScheduleDbContext>(options => options.UseSqlServer(connection));
+
+//Services
+builder.Services.AddScoped<ISelectionService, SelectionService>();
 
 var app = builder.Build();
 
