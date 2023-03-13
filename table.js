0a20e8baeb2ea63b9bdc8e2632086b9add1bcb79
Arina Gavrilenko <rinaaruku@gmail.com>
2023-03-13 13:50:16 +0700

    Исправлен js - Корректно подключён файл js - Генерация дат и дней недели

--- table.js
+++ table.js
@@ -1,10 +1,8 @@
-let monday = new Date();
-let curNumDay = getDate();
-let curMonth = getMonth();
-let curDay = getDay();
 
-function convertDay()
-{switch (curDay)
+function convertDay(curDay)
+{
+
+    switch (curDay)
 {
 
     case 1:
@@ -27,7 +25,9 @@ function convertDay()
     case 6:
         curDay = "СБ"
         break;
-}
+
+    }
+    return curDay;
 }
 
 function getMonday()
@@ -36,13 +36,8 @@ function getMonday()
     let shift = [6, 0, 1, 2, 3, 4, 5];
     monday.setDate(monday.getDate()-shift[monday.getDay()]);
     monday.toLocaleString;
-    let dateNum = {
-        day: monday.getDay(),
-        date: monday.getDate(),
-        month: monday.getMonth
 
-    }
-    return dateNum;
+    return monday;
 }
 
 /*
@@ -53,15 +48,35 @@ table.querySelectorAll('th');
 
 let tbl = [];
 tbl [0] = [];
+headers = [];
+
+let monday = getMonday();
+let dateNum = new Date(monday);
 
 for (let i = 1; i < 7; i++)
 {
-   tbl[0][i] = {
-    day: convertDay(getMonday(dateNum.day)),
-    date: getMonday(dateNum.date),
-    month: getMonday(dateNum.month)
+    let day = convertDay(dateNum.getDay());
+    let date = dateNum.getDate();
+    let month = dateNum.getMonth() + 1;
 
+    headers[i] = {
+    day: day,
+    date: date,
+    month: month, 
+    fulldate: new Date(dateNum)
    }
+   dateNum.setDate(dateNum.getDate()+1);
 
 }
 
+setTableHeders();
+
+function setTableHeders()
+{
+    let table = document.querySelector('table');
+    let ths = table.querySelectorAll('th');
+    for (let i = 1; i < 7; i++)
+    {
+        ths[i].innerHTML = `${headers[i].day}<br>${headers[i].fulldate.toLocaleDateString()}`;
+    }
+}
